266234bd799901319e4f06ef4e82373281e4aea6
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2021-06-13 15:36:45 -0300

    Updated 'text section' component
    [pt-br] Componente 'sessão de texto' alterado

--- lib/components/text_section.dart
+++ lib/components/text_section.dart
@@ -9,32 +9,24 @@ class TextSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        SelectableText(
-          title,
-          style: Theme.of(context).textTheme.headline3,
-        ),
-        SizedBox(height: 16.0),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: content
-              .map(
-                (item) => SelectableText.rich(
-                  TextSpan(
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: "${item['label']}: ",
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      TextSpan(text: item['text']),
-                    ],
-                  ),
+    return SelectableText.rich(
+      TextSpan(
+        children: content
+            .map(
+              (item) => TextSpan(children: [
+                TextSpan(
+                  text: "${item['label']}: ",
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-              )
-              .toList(),
-        )
-      ],
+                TextSpan(
+                  text: item['text'],
+                ),
+                TextSpan(text: '\n')
+              ]),
+            )
+            .toList(),
+      ),
+      style: Theme.of(context).textTheme.bodyText1,
     );
   }
 }
--- lib/pages/measurement/measurement_page.dart
+++ lib/pages/measurement/measurement_page.dart
@@ -1,3 +1,4 @@
+import 'package:desafio_mi/components/text_section.dart';
 import 'package:flutter/material.dart';
 import 'package:desafio_mi/components/app_body.dart';
 import 'package:desafio_mi/pages/select_gas_pumps/widgets/gas_pumps_keyboard_form.dart';
@@ -7,9 +8,40 @@ class MeasurementPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return AppBody(
       child: Container(
-          padding: EdgeInsets.all(24.0),
-          width: 560.0,
-          child: GasPumpsKeyboard(title: 'title')),
+        padding: EdgeInsets.all(24.0),
+        width: 560.0,
+        child: Wrap(
+          children: <Widget>[
+            TextSection(
+              title: 'Dados do Estabelecimento',
+              content: [
+                {'label': 'Rua', 'text': 'Vestibulum et erat'},
+                {'label': 'Bairro', 'text': 'Nunc aliquet ullamcorper'},
+                {'label': 'Cidade', 'text': 'Maecenas'},
+                {'label': 'CEP', 'text': '000.000.000'}
+              ],
+            ),
+            TextSection(
+              title: 'Dados do Abastecimento',
+              content: [
+                {'label': 'Rua', 'text': 'Vestibulum et erat'},
+                {'label': 'Bairro', 'text': 'Nunc aliquet ullamcorper'},
+                {'label': 'Cidade', 'text': 'Maecenas'},
+                {'label': 'CEP', 'text': '000.000.000'}
+              ],
+            ),
+            TextSection(
+              title: 'Dados de Autenticidade',
+              content: [
+                {'label': 'Rua', 'text': 'Vestibulum et erat'},
+                {'label': 'Bairro', 'text': 'Nunc aliquet ullamcorper'},
+                {'label': 'Cidade', 'text': 'Maecenas'},
+                {'label': 'CEP', 'text': '000.000.000'}
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
